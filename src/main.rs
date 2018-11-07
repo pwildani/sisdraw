bed4ad0772af95715c1dfe88ece540d0f8a22775
Peter Wildani <pabw00@gmail.com>
2018-11-06 18:58:00 -0800

    modify imageproc line drawer to have subpixel resolution

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ extern crate serde_derive;
 #[macro_use]
 extern crate clap;
 
+use std::mem::swap;
 use std::cmp::max;
 use std::error::Error;
 use std::ffi::OsString;
@@ -17,7 +18,7 @@ use std::process;
 use clap::{App, Arg};
 
 use image::*;
-use imageproc::drawing::draw_antialiased_line_segment_mut;
+//use imageproc::drawing::draw_antialiased_line_segment_mut;
 use imageproc::pixelops::interpolate;
 
 arg_enum!{
@@ -105,11 +106,11 @@ pub fn dist<T: Into<(f64, f64)>>(a: T, b: T) -> f64 {
 }
 
 impl XY {
-    fn on(&self, dims: (u32, u32)) -> (i32, i32) {
+    fn on(&self, dims: (u32, u32)) -> (f32, f32) {
         let (w, h) = dims;
         let w2 = w as f64 / 2.0;
         let h2 = h as f64 / 2.0;
-        ((w2 + self.x * w2) as i32, (h2 + self.y * h2) as i32)
+        ((w2 + self.x * w2) as f32, (h2 + self.y * h2) as f32)
     }
 }
 
@@ -152,7 +153,7 @@ impl ArcToXYIter {
 }
 
 impl Iterator for ArcToXYIter {
-    type Item = ((i32, i32), (i32, i32));
+    type Item = ((f32, f32), (f32, f32));
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.start == self.end {
@@ -234,56 +235,58 @@ fn draw_fat_arc<I, B>(
         let (x1, y1) = s1;
         let (x2, y2) = s2;
         let slope = (x2 - x1) as f32 / (y2 - y1) as f32;
+        let bgc = |_| bgcolor;
+        let cc = |_| centercolor;
 
         if -1.0 <= slope && slope < 1.0 {
-            draw_antialiased_line_segment_mut(img, (x1 - 3, y1), (x2 - 3, y2), bgcolor, &blend);
-            draw_antialiased_line_segment_mut(img, (x1 + 3, y1), (x2 + 3, y2), bgcolor, &blend);
-            draw_antialiased_line_segment_mut(img, (x1 - 2, y1), (x2 - 2, y2), bgcolor, &blend);
-            draw_antialiased_line_segment_mut(img, (x1 + 2, y1), (x2 + 2, y2), bgcolor, &blend);
-            draw_antialiased_line_segment_mut(img, (x1 - 1, y1), (x2 - 1, y2), bgcolor, &blend);
-            draw_antialiased_line_segment_mut(img, (x1 + 1, y1), (x2 + 1, y2), bgcolor, &blend);
+            draw_gradient_antialiased_line_segment_mut(img, (x1 - 3.0, y1), (x2 - 3.0, y2), bgc, &blend);
+            draw_gradient_antialiased_line_segment_mut(img, (x1 + 3.0, y1), (x2 + 3.0, y2), bgc, &blend);
+            draw_gradient_antialiased_line_segment_mut(img, (x1 - 2.0, y1), (x2 - 2.0, y2), bgc, &blend);
+            draw_gradient_antialiased_line_segment_mut(img, (x1 + 2.0, y1), (x2 + 2.0, y2), bgc, &blend);
+            draw_gradient_antialiased_line_segment_mut(img, (x1 - 1.0, y1), (x2 - 1.0, y2), bgc, &blend);
+            draw_gradient_antialiased_line_segment_mut(img, (x1 + 1.0, y1), (x2 + 1.0, y2), bgc, &blend);
 
-            draw_antialiased_line_segment_mut(img, (x1 - 1, y1), (x2 - 1, y2), centercolor, &blend);
-            draw_antialiased_line_segment_mut(img, (x1 + 1, y1), (x2 + 1, y2), centercolor, &blend);
+            draw_gradient_antialiased_line_segment_mut(img, (x1 - 1.0, y1), (x2 - 1.0, y2), cc, &blend);
+            draw_gradient_antialiased_line_segment_mut(img, (x1 + 1.0, y1), (x2 + 1.0, y2), cc, &blend);
         }
 
         if slope < -1.0 || 1.0 < slope {
-            draw_antialiased_line_segment_mut(img, (x1, y1 + 3), (x2, y2 + 3), bgcolor, &blend);
-            draw_antialiased_line_segment_mut(img, (x1, y1 - 3), (x2, y2 - 3), bgcolor, &blend);
-            draw_antialiased_line_segment_mut(img, (x1, y1 + 2), (x2, y2 + 2), bgcolor, &blend);
-            draw_antialiased_line_segment_mut(img, (x1, y1 - 2), (x2, y2 - 2), bgcolor, &blend);
-            draw_antialiased_line_segment_mut(img, (x1, y1 + 1), (x2, y2 + 1), bgcolor, &blend);
-            draw_antialiased_line_segment_mut(img, (x1, y1 - 1), (x2, y2 - 1), bgcolor, &blend);
+            draw_gradient_antialiased_line_segment_mut(img, (x1, y1 + 3.0), (x2, y2 + 3.0), bgc, &blend);
+            draw_gradient_antialiased_line_segment_mut(img, (x1, y1 - 3.0), (x2, y2 - 3.0), bgc, &blend);
+            draw_gradient_antialiased_line_segment_mut(img, (x1, y1 + 2.0), (x2, y2 + 2.0), bgc, &blend);
+            draw_gradient_antialiased_line_segment_mut(img, (x1, y1 - 2.0), (x2, y2 - 2.0), bgc, &blend);
+            draw_gradient_antialiased_line_segment_mut(img, (x1, y1 + 1.0), (x2, y2 + 1.0), bgc, &blend);
+            draw_gradient_antialiased_line_segment_mut(img, (x1, y1 - 1.0), (x2, y2 - 1.0), bgc, &blend);
 
-            draw_antialiased_line_segment_mut(img, (x1, y1 + 1), (x2, y2 + 1), centercolor, &blend);
-            draw_antialiased_line_segment_mut(img, (x1, y1 - 1), (x2, y2 - 1), centercolor, &blend);
+            draw_gradient_antialiased_line_segment_mut(img, (x1, y1 + 1.0), (x2, y2 + 1.0), cc, &blend);
+            draw_gradient_antialiased_line_segment_mut(img, (x1, y1 - 1.0), (x2, y2 - 1.0), cc, &blend);
         }
 
         if slope == 1.0 {
-            draw_antialiased_line_segment_mut(img, (x1 - 3, y1), (x2 - 3, y2), bgcolor, &blend);
-            draw_antialiased_line_segment_mut(img, (x1 + 3, y1), (x2 + 3, y2), bgcolor, &blend);
-            draw_antialiased_line_segment_mut(img, (x1 - 2, y1), (x2 - 2, y2), bgcolor, &blend);
-            draw_antialiased_line_segment_mut(img, (x1 + 2, y1), (x2 + 2, y2), bgcolor, &blend);
-            draw_antialiased_line_segment_mut(img, (x1 - 1, y1), (x2 - 1, y2), bgcolor, &blend);
-            draw_antialiased_line_segment_mut(img, (x1 + 1, y1), (x2 + 1, y2), bgcolor, &blend);
+            draw_gradient_antialiased_line_segment_mut(img, (x1 - 3.0, y1), (x2 - 3.0, y2), bgc, &blend);
+            draw_gradient_antialiased_line_segment_mut(img, (x1 + 3.0, y1), (x2 + 3.0, y2), bgc, &blend);
+            draw_gradient_antialiased_line_segment_mut(img, (x1 - 2.0, y1), (x2 - 2.0, y2), bgc, &blend);
+            draw_gradient_antialiased_line_segment_mut(img, (x1 + 2.0, y1), (x2 + 2.0, y2), bgc, &blend);
+            draw_gradient_antialiased_line_segment_mut(img, (x1 - 1.0, y1), (x2 - 1.0, y2), bgc, &blend);
+            draw_gradient_antialiased_line_segment_mut(img, (x1 + 1.0, y1), (x2 + 1.0, y2), bgc, &blend);
 
-            draw_antialiased_line_segment_mut(img, (x1 - 1, y1), (x2 - 1, y2), centercolor, &blend);
-            draw_antialiased_line_segment_mut(img, (x1 + 1, y1), (x2 + 1, y2), centercolor, &blend);
+            draw_gradient_antialiased_line_segment_mut(img, (x1 - 1.0, y1), (x2 - 1.0, y2), cc, &blend);
+            draw_gradient_antialiased_line_segment_mut(img, (x1 + 1.0, y1), (x2 + 1.0, y2), cc, &blend);
         }
         if slope == -1.0 {
-            draw_antialiased_line_segment_mut(img, (x1, y1 - 3), (x2, y2 - 3), bgcolor, &blend);
-            draw_antialiased_line_segment_mut(img, (x1, y1 + 3), (x2, y2 + 3), bgcolor, &blend);
-            draw_antialiased_line_segment_mut(img, (x1, y1 - 2), (x2, y2 - 2), bgcolor, &blend);
-            draw_antialiased_line_segment_mut(img, (x1, y1 + 2), (x2, y2 + 2), bgcolor, &blend);
-            draw_antialiased_line_segment_mut(img, (x1, y1 - 1), (x2, y2 - 1), bgcolor, &blend);
-            draw_antialiased_line_segment_mut(img, (x1, y1 + 1), (x2, y2 + 1), bgcolor, &blend);
+            draw_gradient_antialiased_line_segment_mut(img, (x1, y1 - 3.0), (x2, y2 - 3.0), bgc, &blend);
+            draw_gradient_antialiased_line_segment_mut(img, (x1, y1 + 3.0), (x2, y2 + 3.0), bgc, &blend);
+            draw_gradient_antialiased_line_segment_mut(img, (x1, y1 - 2.0), (x2, y2 - 2.0), bgc, &blend);
+            draw_gradient_antialiased_line_segment_mut(img, (x1, y1 + 2.0), (x2, y2 + 2.0), bgc, &blend);
+            draw_gradient_antialiased_line_segment_mut(img, (x1, y1 - 1.0), (x2, y2 - 1.0), bgc, &blend);
+            draw_gradient_antialiased_line_segment_mut(img, (x1, y1 + 1.0), (x2, y2 + 1.0), bgc, &blend);
 
-            draw_antialiased_line_segment_mut(img, (x1, y1 + 1), (x2, y2 + 1), centercolor, &blend);
-            draw_antialiased_line_segment_mut(img, (x1, y1 - 1), (x2, y2 - 1), centercolor, &blend);
+            draw_gradient_antialiased_line_segment_mut(img, (x1, y1 + 1.0), (x2, y2 + 1.0), cc, &blend);
+            draw_gradient_antialiased_line_segment_mut(img, (x1, y1 - 1.0), (x2, y2 - 1.0), cc, &blend);
         }
 
-        draw_antialiased_line_segment_mut(img, s1, s2, centercolor, &blend);
-        draw_antialiased_line_segment_mut(img, s1, s2, centercolor, &blend);
+        draw_gradient_antialiased_line_segment_mut(img, s1, s2, cc, &blend);
+        draw_gradient_antialiased_line_segment_mut(img, s1, s2, cc, &blend);
     }
 }
 
@@ -422,5 +425,113 @@ fn main() {
     if let Err(err) = run(config) {
         println!("{}", err);
         process::exit(1);
+    }
+}
+
+
+pub fn draw_gradient_antialiased_line_segment_mut<I, B, C>(
+    image: &mut I,
+    start: (f32, f32),
+    end: (f32, f32),
+    color: C,
+    blend: B,
+) where
+    I: GenericImage,
+    I::Pixel: 'static,
+    B: Fn(I::Pixel, I::Pixel, f32) -> I::Pixel,
+    C: Fn(f32) -> I::Pixel,
+{
+    let (mut x0, mut y0) = (start.0, start.1);
+    let (mut x1, mut y1) = (end.0, end.1);
+
+    let is_steep = (y1 - y0).abs() > (x1 - x0).abs();
+
+    if is_steep {
+        if y0 > y1 {
+            swap(&mut x0, &mut x1);
+            swap(&mut y0, &mut y1);
+        }
+        let plotter = Plotter {
+            image: image,
+            transform: |x, y| (y, x),
+            blend: blend,
+        };
+        plot_wu_line(plotter, (y0, x0), (y1, x1), color);
+    } else {
+        if x0 > x1 {
+            swap(&mut x0, &mut x1);
+            swap(&mut y0, &mut y1);
+        }
+        let plotter = Plotter {
+            image: image,
+            transform: |x, y| (x, y),
+            blend: blend,
+        };
+        plot_wu_line(plotter, (x0, y0), (x1, y1), color);
+    };
+}
+
+fn plot_wu_line<I, T, B, C>(
+    mut plotter: Plotter<I, T, B>,
+    start: (f32, f32),
+    end: (f32, f32),
+    color: C,
+) where
+    I: GenericImage,
+    I::Pixel: 'static,
+    T: Fn(f32, f32) -> (f32, f32),
+    C: Fn(f32) -> I::Pixel,
+    B: Fn(I::Pixel, I::Pixel, f32) -> I::Pixel,
+{
+    let dx = end.0 - start.0;
+    let dy = end.1 - start.1;
+    let gradient = dy as f32 / dx as f32;
+    let mut fy = start.1 as f32;
+
+    let offset = start.0 - (start.0 as u32) as f32;
+    for xi in (start.0 as u32) ..(end.0 as u32 + 1) {
+        let x = xi as f32 + offset;
+        let frac = x as f32 / (start.0 - end.0 + 1.0) as f32;
+        let c = color(frac);
+        plotter.plot(x, fy as f32, c, 1.0 - fy.fract());
+        plotter.plot(x, fy as f32 + 1.0, c, fy.fract());
+        fy += gradient;
+    }
+}
+
+struct Plotter<'a, I: 'a, T, B>
+where
+    I: GenericImage,
+    I::Pixel: 'static,
+    T: Fn(f32, f32) -> (f32, f32),
+    B: Fn(I::Pixel, I::Pixel, f32) -> I::Pixel,
+{
+    image: &'a mut I,
+    transform: T,
+    blend: B,
+}
+
+impl<'a, I, T, B> Plotter<'a, I, T, B>
+where
+    I: GenericImage,
+    I::Pixel: 'static,
+    T: Fn(f32, f32) -> (f32, f32),
+    B: Fn(I::Pixel, I::Pixel, f32) -> I::Pixel,
+{
+    fn in_bounds(&self, x: f32, y: f32) -> bool {
+        x >= 0.0 && x < self.image.width() as f32 && y >= 0.0 && y < self.image.height() as f32
+    }
+
+    pub fn plot(&mut self, x: f32, y: f32, line_color: I::Pixel, line_weight: f32) {
+        let (x_trans, y_trans) = (self.transform)(x, y);
+        if self.in_bounds(x_trans, y_trans) {
+            let original = self.image.get_pixel(x_trans as u32, y_trans as u32);
+            let blended = (self.blend)(line_color, original, line_weight);
+            self.image.put_pixel(
+                x_trans as u32,
+                y_trans as u32,
+                blended,
+            );
+        }
     }
 }
